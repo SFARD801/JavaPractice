3e5a84670cb8f192ffefa7462789ab3fdb41431f
Sadra Nakhlestani <nsadra4@gmail.com>
2022-11-08 11:14:24 +0330

    Initial commit

---
+++ src/Navigator.java
@@ -0,0 +1,11 @@
+/**
+ * This class Navigate us to the target classes.
+ */
+public class Navigator {
+    /**
+     * Let's goo :)
+     */
+    public static void main(String[] args) {
+        System.out.println("Navigator");
+    }
+}
